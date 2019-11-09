c3698e2fc57ecda0f59b7bc766d679b5e7b6a328
ishan <ishan365.ia@gmail.com>
2019-11-10 02:29:36 +0530

    commit

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,45 @@
+;;makes it necessary to load emacs
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;;automatic theme install for emacs
+(unless (package-installed-p 'solarized-theme)
+  (package-refresh-contents)
+  (package-install 'solarized-theme))
+;;automatic install for .emacs macro
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+;;Loading stuff from org mode file
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+
+;;::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (solarized-gruvbox-dark)))
+ '(custom-safe-themes
+   (quote
+    ("6bf841f77d5eb01455d82ae436e3e25277daaef4ee855a3572589dad1b3ac4b3" default)))
+ '(package-selected-packages (quote (avy which-key use-package solarized-theme))))
+
+ 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
