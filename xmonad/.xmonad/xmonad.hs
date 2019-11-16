6acafabc7922f4ec7499a9b6bfa1202b2a273f17
Ishan <ishan365.ia@gmail.com>
2019-11-16 14:59:00 +0530

    fixed wallpaper xmonad

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -3,22 +3,28 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.SpawnOnce
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+
+myBorderWidth    = 2
+myTerminal = "kitty"
+myModMask = mod4Mask 
+ 
 main = do
   xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-  xmonad $ defaultConfig
-    { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    , borderWidth    = 2
-    , terminal       = "kitty"
-    , modMask = mod4Mask }
- 
-startupHook = do
-  spawnOnce "/usr/bin/nitrogen --restore" 
+  xmonad $ ewmh desktopConfig 
+    { manageHook = manageDocks <+> manageHook desktopConfig
+    , layoutHook = avoidStruts  $  layoutHook desktopConfig
+    , modMask = myModMask
+    , terminal = myTerminal
+    , startupHook = myStartUpHook
+    , borderWidth = myBorderWidth }
+myStartUpHook = do
+  spawnOnce "feh --bg-scale ~/Downloads/the-shadow-of-a-mountain-3440×1440.jpg"
   setWMName "LG3D"
   
 
