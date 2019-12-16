de8b25288653b520618ab5d054ceca5c8a641acb
ishan <ishan365.ia@gmail.com>
2019-12-16 15:25:02 +0530

    colors

---
+++ wal/.cache/wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#272822";
+static const char* selfgcolor   = "#f9f8f5";
+static const char* normbgcolor  = "#a6e22e";
+static const char* normfgcolor  = "#f9f8f5";
+static const char* urgbgcolor   = "#f92672";
+static const char* urgfgcolor   = "#f9f8f5";
