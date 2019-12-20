2d7f979ef1298d211052379803680d905aa47c29
ishan <ishan365.ia@gmail.com>
2019-12-21 01:30:39 +0530

    comit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,9 +6,9 @@
 (package-initialize)
 
 ;;automatic theme install for emacs
-(unless (package-installed-p 'spacemacs-theme)
+(unless (package-installed-p 'darkokai-theme)
   (package-refresh-contents)
-  (package-install 'spacemacs-theme))
+  (package-install 'darkokai-theme))
 ;;automatic install for .emacs macro
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -25,89 +25,3 @@
 ;;::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#689d6a")
- '(cua-normal-cursor-color "#a89984")
- '(cua-overwrite-cursor-color "#d79921")
- '(cua-read-only-cursor-color "#98971a")
- '(custom-enabled-themes (quote (spacemacs-dark)))
- '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(fci-rule-color "#32302f")
- '(highlight-changes-colors (quote ("#d3869b" "#b16286")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#282828" 0.25)
-    (quote
-     ("#d79921" "#689d6a" "#fb4933" "#b16286" "#98971a" "#d65d0e" "#458588"))))
- '(highlight-symbol-foreground-color "#bdae93")
- '(highlight-tail-colors
-   (quote
-    (("#32302f" . 0)
-     ("#747400" . 20)
-     ("#2e7d33" . 30)
-     ("#14676b" . 50)
-     ("#a76e00" . 60)
-     ("#a53600" . 70)
-     ("#9f4d64" . 85)
-     ("#32302f" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#a76e00" "#a53600" "#b21b0a" "#9f4d64" "#8b2a58" "#14676b" "#2e7d33" "#747400")))
- '(hl-fg-colors
-   (quote
-    ("#282828" "#282828" "#282828" "#282828" "#282828" "#282828" "#282828" "#282828")))
- '(hl-paren-colors (quote ("#689d6a" "#d79921" "#458588" "#b16286" "#98971a")))
- '(nrepl-message-colors
-   (quote
-    ("#fb4933" "#d65d0e" "#d79921" "#747400" "#b9b340" "#14676b" "#689d6a" "#d3869b" "#b16286")))
- '(package-selected-packages
-   (quote
-    (gruvbox-theme yaml-mode spaceline yasnippet-snippets which-key use-package spacemacs-theme solarized-theme rainbow-mode magit haskell-mode evil avy)))
- '(pos-tip-background-color "#32302f")
- '(pos-tip-foreground-color "#bdae93")
- '(smartrep-mode-line-active-bg (solarized-color-blend "#98971a" "#32302f" 0.2))
- '(term-default-bg-color "#282828")
- '(term-default-fg-color "#a89984")
- '(vc-annotate-background nil)
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#fb4933")
-     (40 . "#ea9077602ba7")
-     (60 . "#e13d890d26e5")
-     (80 . "#d79921")
-     (100 . "#c25f98e11e8d")
-     (120 . "#b7f4989b1d5e")
-     (140 . "#ad7198331c37")
-     (160 . "#a2cd97aa1b17")
-     (180 . "#98971a")
-     (200 . "#8b4b99073ab0")
-     (220 . "#84009a084776")
-     (240 . "#7bdf9b065366")
-     (260 . "#72aa9c035ed9")
-     (280 . "#689d6a")
-     (300 . "#5c5b933c7677")
-     (320 . "#55928e6e7c70")
-     (340 . "#4de689af8246")
-     (360 . "#458588"))))
- '(vc-annotate-very-old-color nil)
- '(weechat-color-list
-   (quote
-    (unspecified "#282828" "#32302f" "#b21b0a" "#fb4933" "#747400" "#98971a" "#a76e00" "#d79921" "#14676b" "#458588" "#9f4d64" "#d3869b" "#2e7d33" "#689d6a" "#a89984" "#282828")))
- '(xterm-color-names
-   ["#32302f" "#fb4933" "#98971a" "#d79921" "#458588" "#d3869b" "#689d6a" "#a89984"])
- '(xterm-color-names-bright
-   ["#282828" "#d65d0e" "#7c6f64" "#282828" "#a89984" "#b16286" "#bdae93" "#fbf1c7"]))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
