9d79eefb5de8109bc8ef52b353d40efc369a6a49
ishan <ishan365.ia@gmail.com>
2019-12-25 21:41:38 +0530

    emacs improvements

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,8 +13,7 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-
+(global-hl-line-mode 1)
 ;;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
@@ -28,12 +27,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(display-line-numbers-type (quote relative))
- '(global-display-line-numbers-mode t)
- '(tool-bar-mode nil))
+ '(package-selected-packages
+   (quote
+    (all-the-icons org-bullets slime-company slime company-jedi company-irony company-c-headers flycheck-clang-analyzer company avy pretty-mode spaceline rainbow-mode rainbow-delimiters magit yasnippet-snippets yasnippet flycheck evil which-key dashboard helm async beacon use-package darkokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:inherit nil :stipple nil :background "#1d2021" :foreground "#d5c4a1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "CTDB" :family "Fira Code Medium "))))
+ '(cursor ((t (:background "white smoke"))))
+ '(hl-line ((t (:inherit highlight :background "dark slate gray")))))
