a2321f6286b0f4c1bd0a96659cc2e846dce68acc
ishan <ishan365.ia@gmail.com>
2019-12-26 11:53:37 +0530

    removed xmonad

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -29,12 +29,13 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (all-the-icons org-bullets slime-company slime company-jedi company-irony company-c-headers flycheck-clang-analyzer company avy pretty-mode spaceline rainbow-mode rainbow-delimiters magit yasnippet-snippets yasnippet flycheck evil which-key dashboard helm async beacon use-package darkokai-theme))))
+    (treemacs-magit treemacs-evil treemacs all-the-icons org-bullets slime-company slime company-jedi company-irony company-c-headers flycheck-clang-analyzer company avy pretty-mode spaceline rainbow-mode rainbow-delimiters magit yasnippet-snippets yasnippet flycheck evil which-key dashboard helm async beacon use-package darkokai-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#1d2021" :foreground "#d5c4a1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "CTDB" :family "Fira Code Medium "))))
+ '(bold ((t (:weight bold :family "Fira Code"))))
  '(cursor ((t (:background "white smoke"))))
  '(hl-line ((t (:inherit highlight :background "dark slate gray")))))
