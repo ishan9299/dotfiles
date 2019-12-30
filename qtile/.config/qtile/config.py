59d4c9b771734cc572e02a5b796d7b1bfab1083c
ishan <ishan365.ia@gmail.com>
2019-12-31 02:54:48 +0530

    qtile

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -25,10 +25,11 @@
 # SOFTWARE.
 
 from libqtile.config import Key, Screen, Group, Drag, Click
-from libqtile.lazy import lazy
+from libqtile.command import lazy
 from libqtile import layout, bar, widget
 
 from typing import List  # noqa: F401
+from libqtile.config import ScratchPad, DropDown
 
 mod = "mod4"
 
@@ -52,7 +53,7 @@ keys = [
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
     Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
-    Key([mod], "Return", lazy.spawn("tilix")),
+    Key([mod], "Return", lazy.spawn("alacritty")),
 
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout()),
@@ -60,10 +61,10 @@ keys = [
 
     Key([mod, "control"], "r", lazy.restart()),
     Key([mod, "control"], "q", lazy.shutdown()),
-    Key([mod], "r", lazy.spawn("rofi -show drun")),
+    Key([mod], "r", lazy.spawncmd()),
 ]
 
-groups = [Group(i) for i in "asdfuiop"]
+groups = [Group(i) for i in "1234567"]
 
 for i in groups:
     keys.extend([
@@ -71,30 +72,16 @@ for i in groups:
         Key([mod], i.name, lazy.group[i.name].toscreen()),
 
         # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
     ])
 
 layouts = [
     layout.Max(),
-    layout.Stack(num_stacks=2),
-    # Try more layouts by unleashing below layouts.
-    # layout.Bsp(),
-    # layout.Columns(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Stack(num_stacks=2)
 ]
 
 widget_defaults = dict(
-    font='Fira Code',
+    font='Fira Sans',
     fontsize=12,
     padding=3,
 )
@@ -104,14 +91,12 @@ screens = [
     Screen(
         bottom=bar.Bar(
             [
-                widget.CurrentLayout(),
                 widget.GroupBox(),
                 widget.Prompt(),
                 widget.WindowName(),
                 widget.TextBox("default config", name="default"),
                 widget.Systray(),
                 widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.QuickExit(),
             ],
             24,
         ),
@@ -152,12 +137,48 @@ floating_layout = layout.Floating(float_rules=[
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
+# append a scratchpad group
+#groups.append(
+#    ScratchPad(
+#        "scratchpad", [
+#            # define a drop down terminal
+#            # it is placed in the upper third of the screen by default
+#            DropDown(
+#                "term",
+#                "/usr/bin/alacritty",
+#                opacity=0.88,
+#                height=0.55,
+#                width=0.80
+#            ),
 #
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
+#            # define another terminal exclusively for qshell at different position
+#            DropDown(
+#                "qshell",
+#                "/usr/bin/alacritty -e qshell",
+#                x=0.05,
+#                y=0.4,
+#                width=0.9,
+#                height=0.6,
+#                opacity=0.9,
+#                on_focus_lost_hide=True
+#            ),
+#        ]
+#    )
+#)
+#
+## define keys to toggle the dropdown terminals
+#keys.extend([
+#    Key([], "F12", lazy.group["scratchpad"].dropdown_toggle("term")),
+#    Key([], "F11", lazy.group["scratchpad"].dropdown_toggle("qshell")),
+#])
+#
+## XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+## string besides java UI toolkits; you can see several discussions on the
+## mailing lists, GitHub issues, and other WM documentation that suggest setting
+## this string if your java app doesn't work correctly. We may as well just lie
+## and say that we're a working one by default.
+##
+## We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+## java that happens to be on java's whitelist.
+#wmname = "LG3D"
+#
