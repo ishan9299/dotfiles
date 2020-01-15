718d9c7d919b515d9716eb888af10188f5eb8698
ishan <ishan365.ia@gmail.com>
2020-01-16 01:55:25 +0530

    packages for lightdm and slime(emacs)

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 aur()
 {
-    yay --noconfirm --needed -S python-ueberzug tdrop ranger-git brave-bin polybar-git nerd-fonts-fira-code tela-icon-theme-git i3lock-fancy-git
+    yay --noconfirm --needed -S python-ueberzug tdrop ranger-git brave-bin polybar-git nerd-fonts-fira-code tela-icon-theme-git i3lock-fancy-git lightdm-webkit-theme-litarvan
 }
 
 normal()
 {
-    package="bspwm sxhkd nitrogen mate-power-manager feh zsh  xorg-xprop xorg-xwininfo xdotool mpd mpv mpc ncmpcpp alacritty stow rofi"
+    package="bspwm sxhkd nitrogen mate-power-manager feh zsh  xorg-xprop xorg-xwininfo xdotool mpd mpv mpc ncmpcpp alacritty stow rofi sbcl"
     package="$package picom git network-manager-applet ttf-fira-code ttf-fira-sans ttf-font-awesome emacs xorg-xsetroot lxappearance-gtk3"
     package="$package python-pywal dunst gtk-engines llvm clang cmake materia-gtk-theme noto-fonts-emoji xss-lock"
     package="$package lightdm lightdm-webkit2-greeter materia-gtk-theme nvidia nvidia-prime xorg-server poppler zathura highlight zathura-pdf-poppler"
@@ -25,7 +25,7 @@ normal
 
 echo "Creating symlinks"
 cd ~/dotfiles
-stow alacritty dunst lock ncmpcpp rofi bspwm sxhkd doom-emacs mpd mpv polybar ranger picom vim systemd
+stow alacritty dunst lock ncmpcpp rofi bspwm sxhkd emacs mpd mpv polybar ranger picom vim systemd
 
 touch ~/.config/mpd/pid
 touch ~/.config/mpd/log
@@ -33,20 +33,13 @@ touch ~/.config/mpd/playlist
 touch ~/.config/mpd/state
 touch ~/.config/mpd/databse
 systemctl enable --user mpd.service
+systemctl enable --user emacs
 
 if [ ! -d ~/.oh-my-zsh ]
 then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-fi
-
-
-if [ ! -d ~/.doom.d ]
-then
-    git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
-    ~/.emacs.d/bin/doom install
-    systemctl enable --user emacs
 fi
 
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/KaizIqbal/Bibata_Cursor/master/Bibata.sh)"
