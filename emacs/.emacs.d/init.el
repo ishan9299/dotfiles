6c255d77816f22623508aa8ce09ecf7c760cc621
ishan <ishan365.ia@gmail.com>
2020-02-01 23:57:27 +0530

    changed the theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,15 +6,16 @@
 (package-initialize)
 
 ;;automatic theme install for emacs
-(unless (package-installed-p 'spacemacs-theme)
+(unless (package-installed-p 'gruvbox-theme)
   (package-refresh-contents)
-  (package-install 'spacemacs-theme))
+  (package-install 'gruvbox-theme))
 ;;automatic install for .emacs macro
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 (global-hl-line-mode 1)
+(load-theme 'gruvbox t)
 
 ;;:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -30,17 +31,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-enabled-themes (quote (gruvbox-dark-medium)))
  '(custom-safe-themes
    (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+    ("8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" default)))
  '(display-line-numbers-type (quote relative))
  '(global-display-line-numbers-mode t)
  '(helm-autoresize-mode t)
  '(helm-display-source-at-screen-top nil)
+ '(hl-todo-keyword-faces
+   (quote
+    (("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f"))))
  '(package-selected-packages
    (quote
-    (all-the-icons-dired-mode all-the-icons-ivy ivy-rich counsel general neotree winum diminish which-key use-package swiper spacemacs-theme spaceline rainbow-mode rainbow-identifiers rainbow-delimiters pretty-mode org-bullets magit helm flycheck-clang-analyzer evil-org evil-collection dashboard company-irony company-c-headers beacon avy all-the-icons)))
+    (rainbow-identifiers org-bullets slime-company slime company-irony company-c-headers flycheck-clang-analyzer company avy pretty-mode spaceline rainbow-mode rainbow-delimiters magit flycheck evil-org evil which-key all-the-icons-dired all-the-icons-ivy all-the-icons neotree dashboard ivy-hydra ivy-rich counsel hydra general async beacon use-package gruvbox-theme)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 
