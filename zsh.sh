3fcf78dd28622eaae13ec4fc8c4e00de3fc70800
ishan <ishan365.ia@gmail.com>
2020-02-02 21:16:09 +0530

    removed zsh.sh script

--- zsh.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions 
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k 
-    sed -i "s/(git)/(git vi-mode fzf zsh-syntax-highlighting zsh-completions)/;s/\"robbyrussell\"/powerlevel10k\/powerlevel10k/" ~/.zshrc 
-#    echo "_comp_options+=(globdots)" >> ~/.zshrc # autocompletion shows hidden files
