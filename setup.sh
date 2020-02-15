3bcb148f0b1faf253849c20d898fb8f33725a2a4
ishan <ishan365.ia@gmail.com>
2020-02-16 00:09:58 +0530

    configured for my own zsh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ aurwm()
 
 aurgnome()
 {
-    yay --noconfirm -S pop-icon-theme-git python-ueberzug ranger-git ttf-ms-fonts ttf-iosevka lollypop-git nushell
+    yay --noconfirm -S pop-icon-theme-git ttf-ms-fonts ttf-iosevka lollypop-git nushell
 }
 
 # Some pacakges are required by my emacs config like sbcl clang llvm cmake remove these if u dont use emacs 
@@ -21,7 +21,7 @@ bspwm()
     sudo pacman --noconfirm -S $(echo $package)
 }
 
-#ranger is also in gnome it's too convienient
+# ranger is also in gnome it's too convienient
 # added ncmpcpp mpd if you want to show off
 gnome()
 {
@@ -29,7 +29,7 @@ gnome()
     package="$package gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-disk-utility gnome-settings-daemon gufw chromium tlp"
     package="$package gnome-font-viewer gnome-keyring gnome-menus gnome-screenshot gnome-session gnome-shell gnome-themes-extra gnome-user-share pulseaudio-equalizer"
     package="$package gnome-shell-extensions gvfs-google mousetweaks mutter nautilus networkmanager orca rygel sushi tracker tracker-miners vino xdg-user-dirs-gtk gnome-tweaks emacs"
-    package="$package git dosbox xclip dconf alacritty kid3-qt atool avfs noto-fonts-emoji stow mpd mpc ncmpcpp noto-fonts sbcl clang llvm cmake "
+    package="$package git dosbox xclip dconf alacritty kid3-qt atool avfs noto-fonts-emoji stow ranger mpd mpc ncmpcpp noto-fonts sbcl clang llvm cmake "
     package="$package fzf python-pynvim flatpak seahorse-nautilus ttf-font-awesome neovim nvidia ttf-fira-code ttf-fira-sans bumblebee"
     sudo pacman --noconfirm -S $(echo $package)
 }
@@ -53,7 +53,7 @@ then
     cd $HOME/dotfiles
     echo "-Creating Symlinks"
     rm $HOME/.zshrc
-    stow systemd emacs systemd ranger mpv mpd ncmpcpp nvim doom
+    stow systemd emacs systemd ranger mpv mpd ncmpcpp nvim doom zsh
     dconf load / < $HOME/dotfiles/dconf/.config/dconf/user.conf
     systemctl enable --user mpd.service
     systemctl enable --user emacs
@@ -69,7 +69,7 @@ then
     cd $HOME/dotfiles
     echo "-Creating Symlinks" | sed 's/-/ /'
     rm $HOME/.zshrc
-    stow systemd ranger mpv mpd ncmpcpp nvim alacritty polybar bspwm sxhkd rofi wal dunst picom doom
+    stow systemd ranger mpv mpd ncmpcpp nvim alacritty polybar bspwm sxhkd rofi wal dunst picom doom zsh
     dconf load / < $HOME/.config/dconf/user.conf
     systemctl enable --user mpd.service
     systemctl enable --user emacs
@@ -98,21 +98,10 @@ then
     git config --global user.email $email
 fi
 
-if [[ ! -d $HOME/.oh-my-zsh ]]
+if [[ ! -d $HOME/.config/zsh ]]
 then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
-    wait
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
-sed -i "s/(git)/(git vi-mode fzf zsh-syntax-highlighting zsh-completions)/;s/\"robbyrussell\"/powerlevel10k\/powerlevel10k/" $HOME/.zshrc
-
-# showing hidden files
-if grep --quiet "_comp_options+=(globdots)" $HOME/.zshrc
-then
-    echo "already shows hidden files"
-else
-    echo "_comp_options+=(globdots)" >> $HOME/.zshrc # autocompletion shows hidden files
+    mkdir $HOME/.config/zsh
+    cd $HOME/.config/zsh
+    git clone https://github.com/zsh-users/zsh-completions.git
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 fi
