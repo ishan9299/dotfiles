4fbb62b54fc600c2c74cfc841695b43d8be99168
ishan <ishan365.ia@gmail.com>
2020-02-20 19:03:48 +0530

    fedora

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo dnf remove totem libreoffice-* rhythmbox firefox
-sudo dnf install neovim emacs git chromium
+sudo dnf remove totem libreoffice-* rhythmbox firefox gnome-contacts gnome-maps
+sudo dnf install neovim emacs git chromium gnome-tweaks dconf-editor zsh
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     
