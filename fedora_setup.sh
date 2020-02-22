235310fbfad419eda43cb8954633402d43feae7a
ishan <ishan365.ia@gmail.com>
2020-02-22 14:04:30 +0530

    removed ranger configs

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -2,4 +2,5 @@
 sudo dnf remove totem libreoffice-* rhythmbox firefox gnome-contacts gnome-maps
 sudo dnf install neovim emacs git chromium gnome-tweaks dconf-editor zsh
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    
+flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+
