9b1ab43ab14140e8bf981f1200eaf913179f1505
ishan <ishan365.ia@gmail.com>
2020-02-23 12:01:28 +0530

    fedora script complete

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 sudo dnf remove totem libreoffice-* rhythmbox firefox gnome-contacts gnome-maps
-sudo dnf install neovim git chromium gnome-tweaks dconf-editor zsh geany tilix geany-plugins-vimode
+sudo dnf install neovim git chromium gnome-tweaks dconf-editor zsh tilix stow ranger
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+
+# zsh config
+mkdir $HOME/.config/zsh
+cd $HOME/.config/zsh
+sudo curl -fsSL https://startship.rs/install.sh | bash
+git clone https://github.com/zsh-users/zsh-completions.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+
+# Bibata Cursors
+if [[ ! -d /usr/share/icons/Bibata_Amber ]]
+then
+    sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/KaizIqbal/Bibata_Cursor/master/Bibata.sh)"
+fi
 
