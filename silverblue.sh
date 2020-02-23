83695a379303d5e1d9a8e61948f6ea50bafc51e6
ishan <ishan365.ia@gmail.com>
2020-02-24 01:10:33 +0530

    silverblue

---
+++ silverblue.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo rpm-ostree install chromium tilix neovim kid3-qt https://download0.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
