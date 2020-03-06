29f47ab0f23dab0bc42d770e7c78fb50a8f6424c
ishan <ishan365.ia@gmail.com>
2020-03-06 14:24:27 +0530

    just some stuff

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -3,15 +3,32 @@ sudo dnf remove totem libreoffice-* rhythmbox gnome-contacts gnome-maps gnome-bo
 sudo dnf install neovim git gnome-tweaks dconf-editor zsh tilix stow ranger
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+stow ~/dotfiles/zsh
+dconf load / < $HOME/dotfiles/dconf/gnome.config
 
-# zsh config
-mkdir $HOME/.config/zsh
-cd $HOME/.config/zsh
-sudo curl -fsSL https://startship.rs/install.sh | bash
-git clone https://github.com/zsh-users/zsh-completions.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
-wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+if [[ ! -a $HOME/.gitconfig ]]
+then
+    echo "-Enter the name for git " | sed 's/-/ /'
+    read name
+    echo "-Enter the email for git " | sed 's/-/ /'
+    read email
+
+    git config --global user.name $name
+    git config --global user.email $email
+    git config --global core.editor nvim
+fi
+
+if [[ ! -d $HOME/.config/zsh ]]
+then
+    mkdir $HOME/.config/zsh
+    cd $HOME/.config/zsh
+    sudo curl -fsSL https://starship.rs/install.sh | bash
+    git clone https://github.com/zsh-users/zsh-completions.git
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+    wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+    wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+fi
+
 
 # Bibata Cursors
 if [[ ! -d /usr/share/icons/Bibata_Amber ]]
--- scripts/silverblue.sh
+++ scripts/silverblue.sh
@@ -2,4 +2,29 @@
 sudo rpm-ostree install zsh tilix neovim kid3-qt https://download0.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+stow ~/dotfiles/zsh
+dconf load / < $HOME/dotfiles/dconf/gnome.config
+
+if [[ ! -a $HOME/.gitconfig ]]
+then
+    echo "-Enter the name for git " | sed 's/-/ /'
+    read name
+    echo "-Enter the email for git " | sed 's/-/ /'
+    read email
+
+    git config --global user.name $name
+    git config --global user.email $email
+    git config --global core.editor nvim
+fi
+
+if [[ ! -d $HOME/.config/zsh ]]
+then
+    mkdir $HOME/.config/zsh
+    cd $HOME/.config/zsh
+    sudo curl -fsSL https://starship.rs/install.sh | bash
+    git clone https://github.com/zsh-users/zsh-completions.git
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+    wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+    wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+fi
 
