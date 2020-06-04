32d258289951dc71f98cfaa0e9a792e14acd055c
ishan <ishan365.ia@gmail.com>
2020-06-04 11:13:36 +0530

    i dont remember

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,9 @@
 set PATH $HOME/.local/bin $HOME/.local/nvim/bin $HOME/.local/npm/bin $PATH
+set PATH /var/lib/flatpak/exports/bin $PATH
 
 set -gx EDITOR nvim
 
 source $HOME/.config/fish/functions/fzf_key_bindings.fish
+source $HOME/.config/fish/colorschemes/base16-default-dark.fish
 
 starship init fish | source
