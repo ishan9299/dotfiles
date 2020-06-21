a72057a402141c31c10afd0edc8386ee42155547
ishan <ishan365.ia@gmail.com>
2020-06-21 20:01:47 +0530

    nix works in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,4 @@
+
 set PATH $HOME/.local/bin $HOME/.local/nvim/bin $HOME/.local/npm/bin $PATH
 set PATH /var/lib/flatpak/exports/bin $PATH
 
@@ -5,5 +6,6 @@ set -gx EDITOR nvim
 
 source $HOME/.config/fish/functions/fzf_key_bindings.fish
 source $HOME/.config/fish/colorschemes/base16-default-dark.fish
+bass source /home/me/.nix-profile/etc/profile.d/nix.sh
 
 starship init fish | source
