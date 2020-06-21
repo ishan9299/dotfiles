347a7c8a8537446aed7e92441f6d45d7bae4e245
ishan <ishan365.ia@gmail.com>
2020-06-21 10:44:57 +0530

    remove useless scripts

--- scripts/toolbox.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo curl -fsSL https://starship.rs/install.sh | bash
-sudo dnf -y install exa fish fzf htop file clang gcc-c++ npm libX11-xcb glibc-langpack-en fd-find gdb ruby ruby-devel
