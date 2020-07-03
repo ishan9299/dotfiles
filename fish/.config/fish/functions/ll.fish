60431aa8e25d3a2f8c76912a641870765ff46ae1
ishan <ishan365.ia@gmail.com>
2020-07-03 11:01:09 +0530

    remove duplicate functionality

--- fish/.config/fish/functions/ll.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function ll --wraps='exa -abghGHliS' --description 'alias ll=exa -abghGHliS'
-  exa -abghGHliS $argv;
-end
--- fish/.config/fish/functions/ls.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function ls --wraps='exa --long' --description 'alias ls=exa --long'
-  exa --long $argv;
-end
