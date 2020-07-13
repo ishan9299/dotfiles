772e53d512e97b570b8408e1115ea8eb10c488db
ishan <ishan365.ia@gmail.com>
2020-07-13 21:55:41 +0530

    moved fish config to home.nix

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-function fish_user_key_bindings
-    bind \eo 'lfcd'
-    bind \co 'lf'
-    fzf_key_bindings
-end
--- fish/.config/fish/functions/htop.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function htop --wraps='htop -t' --description 'alias htop=htop -t'
- command htop -t $argv;
-end
